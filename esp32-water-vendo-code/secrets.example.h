dec8dfdd4c8a6bb30247fddbd7c5c438e0993e06
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-05-24 01:16:32 +0100

    Update secrets.example.h

--- esp32-water-vendo-code/secrets.example.h
+++ esp32-water-vendo-code/secrets.example.h
@@ -3,8 +3,8 @@
 // Copy this file to secrets.h and fill real device credentials.
 // secrets.h is ignored by git.
 #define WIFI_ENABLED true
-#define WIFI_SSID "Redmi Note 14"
-#define WIFI_PASSWORD "0987654321"
+#define WIFI_SSID "gfdghbs"
+#define WIFI_PASSWORD "zfdxvxc"
 
-#define FIREBASE_PROJECT_ID "pauloheymann-integ-prog"
-#define FIREBASE_API_KEY "AIzaSyD5YGR3JGFmOp51UDHxqFVjIsrSX_i9wpU"
+#define FIREBASE_PROJECT_ID "dzfs-dfg"
+#define FIREBASE_API_KEY "dzgfgfhdbntgfjhtshhbdfb"
